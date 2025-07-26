7e4bc68426ed1443e663ef0da07db2e04160cb36
Alexxus <oleoalexis@gmail.com>
2025-07-26 16:37:50 -0600

    controlador, rutas y para el back

--- Clase1/back1/routes/rutas.routes.js
+++ Clase1/back1/routes/rutas.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const saludosController = require("../controller/saludoController")
 
 router.get("/", saludosController.general)
+
 router.get("/hola", saludosController.saludo)
 
 
